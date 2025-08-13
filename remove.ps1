5c808a9600eb5234d30d619624caf8402c7e1c18
Carlos Adriano <souza.adriano.carlos@gmail.com>
2025-08-12 21:10:37 -0300

    commit 2110 - main

---
+++ remove.ps1
@@ -0,0 +1,5 @@
+#Remove branch
+git push origin --delete hml  ##Remove a branch remota denominada 'hml'
+git branch -D hml ##Remove a branch local denominada hml
+git push origin --delete dev  ##Remove a branch remota denominada 'hml'
+git branch -D dev ##Remove a branch local denominada hml
